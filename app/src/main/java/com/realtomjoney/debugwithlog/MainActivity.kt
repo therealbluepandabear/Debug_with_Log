bdaf55d200dd229ea0f2bb1828e2a978322bd28a
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 01:20:48 +1300

    Added TAG.

--- app/src/main/java/com/realtomjoney/debugwithlog/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/debugwithlog/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.widget.TextView
 import com.realtomjoney.debugwithlog.databinding.ActivityMainBinding
 
+private const val TAG: String = "MyActivity"
+
 class MainActivity : AppCompatActivity() {
     /*
     Log.v // Verbose (print everything out)
@@ -18,15 +20,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     var myLabel: TextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBindings()
 
-        Log.d("MyTag", "Line 25")
+        Log.d(TAG, "Line 25")
         val lbl = myLabel!!
 
-        Log.d("MyTag", "Line 30")
+        Log.d(TAG, "Line 30")
     }
 
     private fun setBindings() {
