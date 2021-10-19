930a59b30326929870e3e3efcb6a77c4e6b9030c
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 01:07:55 +1300

    Simulated crash.

--- app/src/main/java/com/realtomjoney/debugwithlog/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/debugwithlog/MainActivity.kt
@@ -2,14 +2,19 @@ package com.realtomjoney.debugwithlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.realtomjoney.debugwithlog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    var myLabel: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBindings()
+
+        val lbl = myLabel!!
     }
 
     private fun setBindings() {
