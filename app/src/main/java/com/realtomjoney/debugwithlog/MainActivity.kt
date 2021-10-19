929f363b9f0877a042759fd65931b1939c128390
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 01:11:17 +1300

    Logging information.

--- app/src/main/java/com/realtomjoney/debugwithlog/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/debugwithlog/MainActivity.kt
@@ -6,6 +6,14 @@ import android.widget.TextView
 import com.realtomjoney.debugwithlog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /*
+    Log.v // Verbose (print everything out)
+    Log.d // Debug
+    Log.i // Info (successful information)
+    Log.w // Warnings (situations where something that you didn't expect happened, but it's not an error)
+    Log.e // An error
+    Log.wtf // Find whatever Android Studio can find for you
+     */
     private lateinit var binding: ActivityMainBinding
 
     var myLabel: TextView? = null
